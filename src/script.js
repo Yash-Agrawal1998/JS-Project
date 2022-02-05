0a17adfcec07a09e20139f70d7c4bfe31fbef132
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 11:29:05 +0530

    Compound Assignment With Augmented Multiplication

--- src/script.js
+++ src/script.js
@@ -121,6 +121,14 @@ a -= 6;
 b -= 15;
 c -= 1;
 
+//Task 23:Convert the assignments for a, b, and c to use the *= operator
+let a = 5;
+let b = 12;
+let c = 4.6;
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
 
 
 
