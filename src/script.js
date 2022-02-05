34b9c64fc3d1571a7d4cdd29c9fec3f8906a7cd0
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 10:40:27 +0530

    Declare String Variables

--- src/script.js
+++ src/script.js
@@ -28,3 +28,7 @@ b=a;
 
 //Task 5:Define a variable a with var and initialize it to a value of 9.
 var a=9;
+
+//Task 6:Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
+var myFirstName="Yash";
+var myLastName="Agrawal";
