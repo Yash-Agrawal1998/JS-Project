1c3953e48d1a50a2f2a0b5d46f5e21a839418f84
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 12:13:12 +0530

    Find the Length of a String

--- src/script.js
+++ src/script.js
@@ -167,4 +167,11 @@ const someAdjective = "A b c";
 let myStr = "Learning to code is ";
 myStr+=someAdjective;
 
+//Task 32:Use the .length property to count the number of characters in the lastName variable and assign it to lastNameLength.
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+// Only change code below this line
+lastNameLength = lastName.length;
+
 
