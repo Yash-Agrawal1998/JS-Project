62a2e4782a2fa1f44a98d04786d4f288c57a4881
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 10:44:32 +0530

    Understanding Uninitialized Variables

--- src/script.js
+++ src/script.js
@@ -32,3 +32,19 @@ var a=9;
 //Task 6:Create two new string variables: myFirstName and myLastName and assign them the values of your first and last name, respectively.
 var myFirstName="Yash";
 var myLastName="Agrawal";
+
+//Task 7:Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a=5;
+b=10;
+c="I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+
