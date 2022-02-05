d937431dfac0fbf02ae08841a30867b8bacbe33b
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 12:18:42 +0530

    Understand String Immutability

--- src/script.js
+++ src/script.js
@@ -181,4 +181,11 @@ const lastName = "Lovelace";
 // Only change code below this line
 firstLetterOfLastName = lastName[0];
 
+//Task 34:Correct the assignment to myStr so it contains the string value of Hello World using the approach shown in the example above.
+// Setup
+let myStr = "Jello World";
+// Only change code below this line
+myStr= "Hello World";
+
+
 
