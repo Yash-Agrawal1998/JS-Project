079205578bcd24f407d03a8ae007afb0a57a4093
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 11:31:34 +0530

    Compound Assignment With Augmented Division

--- src/script.js
+++ src/script.js
@@ -130,5 +130,13 @@ a *= 5;
 b *= 3;
 c *= 10;
 
+//Task 24:Convert the assignments for a, b, and c to use the /= operator.
+let a = 48;
+let b = 108;
+let c = 33;
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
 
 
