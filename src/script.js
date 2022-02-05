7f5cfff6dee3afcecf2600b7f1d90c2fe182103d
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 12:02:39 +0530

    Concatenating Strings with the Plus Equals Operator

--- src/script.js
+++ src/script.js
@@ -151,5 +151,9 @@ FirstLine
 ThirdLine*/
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
 
-//Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
+//Task 28:Build myStr from the strings This is the start. and This is the end. using the + operator. Be sure to include a space between the two strings.
 const myStr = "This is the start." + " This is the end.";
+
+//Task 29:Build myStr over several lines by concatenating these two strings: This is the first sentence. and This is the second sentence. using the += operator. Use the += operator similar to how it is shown in the example and be sure to include a space between the two strings. Start by assigning the first string to myStr, then add on the second string.
+let myStr="This is the first sentence. ";
+myStr+="This is the second sentence.";
