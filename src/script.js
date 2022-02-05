f6b1e0846fce5d0efdc344e2596a6fbdeef6bc01
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 10:55:28 +0530

    Declare a Read-Only Variable with the const Keyword

--- src/script.js
+++ src/script.js
@@ -62,6 +62,12 @@ titleCaseOver = 9000;
 let catName = "Oliver";
 let catSound = "Meow!";
 
+//Task 10:Change the code so that all variables are declared using let or const. Use let when you want the variable to change, and const when you want the variable to remain constant.
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+
 
 
 
