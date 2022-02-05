672423583aa8b66bf28b7cf92d9f692034ab5a69
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 10:29:04 +0530

    Storing Values with the Assignment Operator

--- src/script.js
+++ src/script.js
@@ -10,3 +10,9 @@ multi-line comment */
 
 //Task 2:Use the var keyword to create a variable called myName.
 var myName;
+
+//Task 3:Assign the value 7 to variable a.
+// Setup
+var a;
+a=7;
+// Only change code below this line
