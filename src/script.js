a985f740957a5e45a29660fae89795c86bffa602
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 11:36:16 +0530

    Escaping Literal Quotes in Strings

--- src/script.js
+++ src/script.js
@@ -139,4 +139,5 @@ a /= 12;
 b /= 4;
 c /= 11;
 
-
+//Task 25:Use backslashes to assign a string to the myStr variable so that if you were to print it to the console, you would see:I am a "double quoted" string inside "double quotes".
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
