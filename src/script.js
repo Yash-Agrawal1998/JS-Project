5cc08afd8f3a66b7d8d63b67fd42d014e6c75756
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 12:56:03 +0530

    Manipulate Arrays With unshift()

--- src/script.js
+++ src/script.js
@@ -257,3 +257,10 @@ var removedFromMyArray=myArray.pop();
 const myArray = [["John", 23], ["dog", 3]];
 // Only change code below this line
 var removedFromMyArray=myArray.shift();
+
+//Task 47:Add ["Paul", 35] to the beginning of the myArray variable using unshift().
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+// Only change code below this line
+myArray.unshift(["Paul", 35]);
