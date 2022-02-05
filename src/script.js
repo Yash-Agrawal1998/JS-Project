3367b2e1391d09d37c2e3a74d204b5e9b457f59f
cedcoss <yashagrawal@cedcoss.com>
2022-02-05 14:41:23 +0530

    Logical Order in If Else Statements

--- src/script.js
+++ src/script.js
@@ -592,3 +592,15 @@ else{return "Between 5 and 10";}
 
 testElseIf(7);
 
+//Task 73:Change the order of logic in the function so that it will return the correct statements in all cases.
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";}
+    else if (val < 10 ) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(4);
